39bf5dd39aa609ae512851294b08b9bbe6bcd6d3
Ataslan Monteiro <ataslan.monteiro@gmail.com>
2024-09-26 10:37:53 -0300

    dn ok
    Co-authored-by: Pedro Euzebio <pedroeuzebio.contato@gmail.com>

--- main/data-nascimento.js
+++ main/data-nascimento.js
@@ -10,47 +10,42 @@
 */
 
 function validarDataNascimento(data) {
-// A gente tem que verificar se a data está no formato dd/mm/aaaa
-  const regex = /^\d{2}\/\d{2}\/\d{4}$/;
+  // Eu vou verifica o padrão do formato dd/mm/aaaa
+  const regex = /^(\d{2})\/(\d{2})\/(\d{4})$/;
   const match = data.match(regex);
 
-  if(!math) {
-    return false;
-  }
-  const dia = parseInt(math[1], 10);
-  const mes = parseInt(math[2], 10);
-  const ano = parsetInt(math[3], 10);
+  // Faço a verificação se não corresponde ao formato
+  if (!match) return false;
 
-  // Aqui verifica se o més é válido 
-  if(mes < 1 || mes > 12) {
-    return false
-  }
-  // aqui eu pego o ano atual usando a função getFullYear()
+  const dia = parseInt(match[1], 10);
+  const mes = parseInt(match[2], 10);
+  const ano = parseInt(match[3], 10);
+
+  // Faço a verificação se o mês é válido (1-12)
+  if (mes < 1 || mes > 12) return false;
+
+  // Verifica se o ano está entre 1904 e o ano atual
   const anoAtual = new Date().getFullYear();
+  if (ano < 1904 || ano > anoAtual) return false;
 
-  // verifica o ano
-  if(ano < 1904 || ano > anoAtual) {
-    return false;
-  }
-
+  // Define os dias máximos de cada mês
   const diasPorMes = [
-    31, // Janeiro (31 dias)
-    (ano % 4 == 0 && (ano % 100 !== 0 || ano % 400 === 0)) ? 29 : 28, // Fevereiro,
-    31, // Março
-    30, // Abril
-    31, // Maio
-    30, // Junho
-    31, // Julho
-    31, // Agosto
-    30, // Setembro
-    31, // Outubro
-    30, // Novembro
-    31, // Dezembro
-  ]
-  
-  if(dia < 1 || dia > diasPorMes[mes - 1]) {
-    return false;
-  }
+    31,
+    ano % 4 === 0 && (ano % 100 !== 0 || ano % 400 === 0) ? 29 : 28,
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31,
+  ];
+
+  // Verifica se o dia é válido para o mês específico
+  if (dia < 1 || dia > diasPorMes[mes - 1]) return false;
 
   return true;
 }
